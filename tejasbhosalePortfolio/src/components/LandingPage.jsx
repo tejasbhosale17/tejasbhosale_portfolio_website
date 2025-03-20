1a595c0d8e0f34b3975a56e1b3570583c6e80973
Tejas Bhosale <tejasvbhosale@gmail.com>
2025-03-20 17:19:38 +0530

    improvised stylings
    improvised styling of profile-img and made navbar and intro responsive

--- tejasbhosalePortfolio/src/components/LandingPage.jsx
+++ tejasbhosalePortfolio/src/components/LandingPage.jsx
@@ -1,14 +1,17 @@
 import profileImg from "../assets/profile-img-1.png";
+import WelcomeText from "./Rough";
+import ScrollVelocity from "./ScrollVelocity";
 
 import "./styles/landingPage.min.css";
 const LandingPage = () => {
+  const velocity = 100;
   return (
     <div className="landing-main">
-      <div className="hero-section" style={{ borderBottom: "1px solid grey" }}>
+      <div className="hero-section">
         <div className="container tag-line">
           <div className="row">
-            <div className="col-6">
-              <h1 class="heading">
+            <div className="col-6 heading-col">
+              <h1 className="heading">
                 The only
                 <br />
                 Tech Talent
@@ -32,6 +35,19 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
+      </div>
+      {/* <div className="welcome-scroll">
+        <ScrollVelocity
+          texts={["WELCOME!"]}
+          velocity={velocity}
+          className="custom-scroll-text"
+        />
+      </div> */}
+      <div className="welcome-section-temp">
+        <WelcomeText text="WELCOME!" speed={8} />
+      </div>
+      <div className="about-section">
+        <WelcomeText text="ABOUT" speed={8} />
       </div>
     </div>
   );
